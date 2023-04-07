e6c939f9eb613527f2ce86b4acb5af3fa194b3aa
dipanshuhappy <dipanshuhappy@gmail.com>
2023-04-07 22:48:11 +0530

    added auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,11 +3,15 @@ import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
+  User,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { Role } from "@prisma/client";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -15,19 +19,19 @@ import { prisma } from "~/server/db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
+
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
       // ...other properties
-      // role: UserRole;
+      role: Role;
     } & DefaultSession["user"];
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+  interface User {
+    role: Role;
+  }
 }
 
 /**
@@ -40,17 +44,53 @@ export const authOptions: NextAuthOptions = {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
+        session.user.email = user.email;
+        session.user.role = user.role;
       }
       return session;
     },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("No credentials found");
+        }
+        const account = await prisma.user.findUnique({
+          where: { email: credentials.email },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            password: true,
+          },
+        });
+
+        if (!account) {
+          throw new Error("No account found");
+        }
+        if (!account.password) {
+          throw new Error("No password found");
+        }
+        const valid = bcrypt.compareSync(
+          credentials?.password,
+          account.password
+        );
+
+        if (!valid) {
+          throw new Error("Invalid password");
+        }
+        const { password, ...user } = account;
+        return user as User;
+      },
     }),
+
     /**
      * ...add more providers here.
      *
