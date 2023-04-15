d0a8f69a2c34532b1d8efb8ca223482adc46d289
swaraj bachu <rajswaraj.r@gmail.com>
2023-04-15 15:36:38 +0530

    styles of adding socials

--- src/components/profile/ConnectSocials.tsx
+++ src/components/profile/ConnectSocials.tsx
@@ -1,68 +1,28 @@
 import React from "react";
 import BasicButton from "./basic_button";
+import { IoIosAddCircle } from "react-icons/io";
 
 const ConnectSocials = () => {
   return (
-    <div className="mt-[10vh]  w-[95%] rounded-xl p-6 shadow-custom">
+    <div className="boxShadowCust  mt-[10vh] h-[50vh] w-[95%] rounded-xl p-6">
       {/* <h1 className='text-5xl font-medium text-gray-600'>Connect Socials</h1> */}
       <div className="mt-4 flex flex-col">
-        {/* <span className='text-3xl font-bold text-gray-700  mb-4'>Twitter</span>
-                <span className='text-md font-normal text-gray-500 mb-3'>Connect your Twitter account to your profile</span>
-                <BasicButton text='Connect'  onClick={()=>{}}/> */}
-        <ConnectBlock
-          heading="Twitter"
-          subheading="Connect your Twitter account to your profile"
-          buttontext="Connect"
-          onClick={() => {}}
-        />{" "}
-        <div className="divider"></div>
-        <ConnectBlock
-          heading="LinkedIn"
-          subheading="Connect your LinkedIn account to your profile"
-          buttontext="Connect"
-          onClick={() => {}}
-        />
-        <div className="divider"></div>
-        <ConnectBlock
-          heading="Instagram"
-          subheading="Connect your Instagram account to your profile"
-          buttontext="Connect"
-          onClick={() => {}}
-        />
-        <div className="divider"></div>
-        <ConnectBlock
-          heading="Facebook"
-          subheading="Connect your Facebook account to your profile"
-          buttontext="Connect"
-          onClick={() => {}}
-        />
-        <div className="divider"></div>
+        <h2 className="text-3xl font-bold">Connect your socials</h2>
+
+        <div className="mt-4 flex gap-4">
+          <AddSocial />
+        </div>
       </div>
     </div>
   );
 };
 
-type ConnectBlockProps = {
-  heading: string;
-  subheading: string;
-  buttontext: string;
-  onClick: () => void;
-};
-
-const ConnectBlock: React.FC<ConnectBlockProps> = ({
-  heading,
-  subheading,
-  buttontext,
-  onClick,
-}) => {
+const AddSocial = () => {
   return (
-    <>
-      <span className="mb-4 text-2xl font-bold  text-gray-700">{heading}</span>
-      <span className="text-md mb-3 font-normal text-gray-500">
-        {subheading}
-      </span>
-      <BasicButton text={buttontext} onClick={onClick} />
-    </>
+    <button className="btn-primary btn gap-2 text-white">
+      <IoIosAddCircle className="text-2xl" />
+      Add Socials
+    </button>
   );
 };
 
