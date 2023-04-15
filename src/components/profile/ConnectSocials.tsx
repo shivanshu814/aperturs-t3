90094a80c5451eb4148fab286abe6fbac8ecfb8c
swaraj bachu <rajswaraj.r@gmail.com>
2023-04-15 15:51:25 +0530

    added popup

--- src/components/profile/ConnectSocials.tsx
+++ src/components/profile/ConnectSocials.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import BasicButton from "./basic_button";
 import { IoIosAddCircle } from "react-icons/io";
+import {FaFacebookSquare,FaLinkedinIn} from 'react-icons/fa'
+import {AiFillInstagram,AiOutlineTwitter,} from 'react-icons/ai'
 
 const ConnectSocials = () => {
   return (
@@ -19,11 +21,52 @@ const ConnectSocials = () => {
 
 const AddSocial = () => {
   return (
-    <button className="btn-primary btn gap-2 text-white">
-      <IoIosAddCircle className="text-2xl" />
-      Add Socials
-    </button>
+    <div>
+      <label htmlFor="my-modal-3" className="btn-primary btn gap-2 text-white">
+        <IoIosAddCircle className="text-2xl" />
+        Add Socials
+      </label>
+
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="my-modal-3"
+            className="btn-sm btn-circle btn absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">
+            Add Socials to Aperturs
+          </h3>
+          <Socials />
+        </div>
+      </div>
+    </div>
   );
 };
+
+const Socials = () => {
+  return (
+    <div className="grid grid-cols-3 py-4 gap-4">
+      <button className="btn hover:bg-primary hover:text-white hover:border-0  gap-2">
+        <FaFacebookSquare className="text-2xl " />
+        <p>Facebook</p>
+      </button>
+      <button className="btn hover:bg-primary hover:text-white hover:border-0  gap-2">
+        <AiOutlineTwitter className="text-2xl " />
+        <p>Twitter</p>
+      </button>
+      <button className="btn hover:bg-primary hover:text-white hover:border-0  gap-2">
+        <AiFillInstagram className="text-2xl " />
+        <p>Insta</p>
+      </button>
+      <button className="btn hover:bg-primary hover:text-white hover:border-0  gap-2">
+        <FaLinkedinIn className="text-2xl " />
+        <p>Linkedin</p>
+      </button>
+    </div>
+  )
+}
 
 export default ConnectSocials;
