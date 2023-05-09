0dfe39abfac102972279e4fdf8851147deeb3747
swaraj bachu <rajswaraj.r@gmail.com>
2023-05-09 13:30:43 +0530

    add twitter fd done

--- src/pages/socials/twitter.tsx
+++ src/pages/socials/twitter.tsx
@@ -1,43 +1,70 @@
-import React from 'react'
+import React from "react";
 
 const AddTwitter = () => {
   return (
-    <div className='py-28 px-56 w-screen'>
-        <h1 className='font-medium text-4xl text-primary'>
-            Add Twitter
-        </h1>
-        <p className='mt-2 text-gray-500'>
-          you will need to give your own 
-          <span className='font-bold text-primary'> Twitter API keys </span>
-        </p>
-        <p className='text-gray-500'>
-          you can get them from Twitter Developer Portal
-        </p>
-        <ApiBox />
-
+    <div className="w-screen lg:px-56 md:px-24 px-10 py-28">
+      <h1 className="text-4xl font-medium text-primary">Add Twitter</h1>
+      <p className="mt-2 text-gray-500">
+        you will need to give your own
+        <span className="font-bold text-primary"> Twitter API keys </span>
+      </p>
+      <p className="text-gray-500">
+        you can get them from Twitter Developer Portal
+      </p>
+      <ApiBox />
     </div>
-  )
+  );
+};
+
+function ApiBox() {
+  return (
+    <div className="postShadow mt-8 flex  w-full flex-col rounded-xl p-6">
+      <h1 className="text-2xl font-medium text-primary">API Keys</h1>
+      <p className="mt-2 text-gray-500">Client ID</p>
+      <input
+        className="my-2 h-auto w-full resize-none rounded-xl border border-primary p-3"
+        placeholder="UE05dTJ45jhjTvdEUYQ5aTBIcFo6MTpjaQ"
+      ></input>
+      <p className="mt-2 text-gray-500">Client Secret</p>
+      <input
+        className="h-auto w-full resize-none rounded-xl  
+      border border-primary p-3 "
+        placeholder="NzEjJmLCdQud4JuKSw2QaLjFv4zSTQWtg31hRwrsdSfw3ayqfq"
+      ></input>
+
+      <div className="alert alert-warning my-3 bg-orange-200 ">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 flex-shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          <div>
+          <span className="font-bold">Bring your Own APIs</span>
+          <span className="m-1">
+            Due to Twitter new APIs rules, we are only allowing user who bring
+            their own Api Keys
+          </span>
+          </div>
+        </div>
+
+      </div>
+      <div className="w-full flex justify-end">
+      <button className="btn btn-primary sm:w-56 w-24  text-white rounded-xl px-4 py-2 mt-4">
+        Save
+      </button>
+      </div>
+      
+    </div>
+  );
 }
 
-
-function ApiBox(){
-  return(
-    <div className='flex postShadow w-full  p-6 rounded-xl mt-8 flex-col'>
-      <textarea
-  className='w-[80%] my-2 h-auto border-primary border resize-none rounded-xl p-3'
-  placeholder='API Key'
-  rows={1}
-  cols={1}
-  maxLength={50}
-></textarea>
-
-
-      <textarea  className='w-[80%] h-auto border-primary border  
-      resize-none rounded-xl p-3 ' placeholder='API secret'></textarea>
-
-     </div>
-  )
-}
-
-
-export default AddTwitter
+export default AddTwitter;
