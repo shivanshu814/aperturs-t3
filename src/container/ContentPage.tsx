5b6394b71274f843cb0580cf0839dac6ee6e1748
swaraj bachu <rajswaraj.r@gmail.com>
2023-05-26 12:07:56 +0530

    removed connect twitter button

--- src/container/ContentPage.tsx
+++ src/container/ContentPage.tsx
@@ -11,28 +11,13 @@ const ContentPage = () => {
   const [bearerToken, setbearerToken] = React.useState("");
   const { session } = useSession();
   const { data } = useNextAuthSession();
-  const { mutateAsync: addSecrets } = api.tweet.addSecrets.useMutation();
-  const onSubmit = async () => {
-    if (session?.user.id) {
-      await addSecrets({ bearerToken, userId: session?.user.id });
-      toast("Secrets Added Successfully");
-    }
-  };
+
   return (
     <div className="m-12 w-full">
       <div className="flex w-full justify-between">
         <h2 className="text-2xl font-bold">Content</h2>
         <div className="flex ">
-          <CreateButton text="Create" />
-          {!data?.user ? (
-            <BasicButton
-              onClick={() => {
-                signIn();
-              }}
-              text={"Connect Twitter"}
-            />
-          ) : null}
-          
+          <CreateButton text="Create" />          
         </div>
       </div>
     </div>
