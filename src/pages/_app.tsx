9b24f38fb7f28f83e725108bc49e1c25d1af3a2b
swaraj bachu <rajswaraj.r@gmail.com>
2023-06-18 13:03:51 +0530

    material tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { ThemeProvider } from "@material-tailwind/react";
+
 
 import { api } from "~/utils/api";
 
@@ -15,8 +17,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <ClerkProvider {...pageProps}>
+      <ThemeProvider>
+
         <Toaster position="bottom-center" reverseOrder={false} />
         <Component {...pageProps} />
+      </ThemeProvider>
       </ClerkProvider>
     </SessionProvider>
   );
