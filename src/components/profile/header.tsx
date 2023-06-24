a10db7f9928597a67a1398139878ba7b9691a6fd
dipanshuhappy <dipanshuhappy@gmail.com>
2023-06-24 18:57:00 +0530

    fixed infally

--- src/components/profile/header.tsx
+++ src/components/profile/header.tsx
@@ -3,43 +3,38 @@ import React from 'react'
 
 const ProfileHeader = () => {
 
-    const {data: session,status:status} = useSession()
-    console.log(session)
 
-    if(status === 'loading') return <div>Loading...</div>
-    if(!session) return <div></div>
-    
 
-  return (
-    <div className='w-full flex flex-col items-center justify-center'>
-    <div className='w-full p-8  rounded-xl h-[30vh] bg-cover bg-center bg-no-repeat'
-    style = {{
-        
-        backgroundImage: 'url(gradbg.png)'
-        //set cover
+    return (
+        <div className='w-full flex flex-col items-center justify-center'>
+            <div className='w-full p-8  rounded-xl h-[30vh] bg-cover bg-center bg-no-repeat'
+                style={{
 
-    }}
-    >
-    </div>
-    <div className='w-[95%] p-4 flex justify-between rounded-full m-[-7vh] glassMorphism '>
-    
-        <div className='flex'>
-            <img className='rounded-xl shadow-md '
-            src={session.user.image ?? 'profile.png'}
-            width={65}
-            height={50}
-            alt='profile'
-            />
-            <div className='flex flex-col justify-center ml-6'>
-                <h1 className='text-xl font-medium text-gray-600'>{session.user.name}</h1>
-                <h1 className='text-sm font-medium text-gray-500'>UI/UX Designer</h1>
+                    backgroundImage: 'url(gradbg.png)'
+                    //set cover
+
+                }}
+            >
             </div>
-        </div>
-        </div>       
+            <div className='w-[95%] p-4 flex justify-between rounded-full m-[-7vh] glassMorphism '>
 
-    </div>
-    
-  )
+                <div className='flex'>
+                    <img className='rounded-xl shadow-md '
+                        src={'profile.png'}
+                        width={65}
+                        height={50}
+                        alt='profile'
+                    />
+                    <div className='flex flex-col justify-center ml-6'>
+                        <h1 className='text-xl font-medium text-gray-600'>Test</h1>
+                        <h1 className='text-sm font-medium text-gray-500'>UI/UX Designer</h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
 }
 
 export default ProfileHeader
--- src/env.mjs
+++ src/env.mjs
@@ -26,12 +26,7 @@ const client = z.object({
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-  TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
-  TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET,
+
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
