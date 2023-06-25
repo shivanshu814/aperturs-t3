fda8bff2d3e8bdbfb106139401ed08cb0cd5b3d0
swaraj bachu <rajswaraj.r@gmail.com>
2023-06-25 18:47:52 +0530

    📦 chore(package.json): add micro and svix packages ✨ feat(user.ts): add webhook endpoint to handle user created and updated events from Clerk The micro package is added to handle the webhook endpoint. The svix package is added to verify the webhook signature. The user.ts file is added to handle the user created and updated events from Clerk. The endpoint verifies the webhook signature, extracts the event data, and upserts the user data into the database.

---
+++ src/pages/api/webhooks/user.ts
@@ -0,0 +1,66 @@
+import { IncomingHttpHeaders } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Webhook, WebhookRequiredHeaders } from "svix";
+import { buffer } from "micro";
+import { prisma } from "~/server/db";
+
+// Disable the bodyParser so we can access the raw
+// request body for verification.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+
+export default async function handler(
+  req: NextApiRequestWithSvixRequiredHeaders,
+  res: NextApiResponse
+) {
+  // Verify the webhook signature
+  // See https://docs.svix.com/receiving/verifying-payloads/how
+  const payload = (await buffer(req)).toString();
+  const headers = req.headers;
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+  try {
+    evt = wh.verify(payload, headers) as Event;
+  } catch (_) {
+    return res.status(400).json({});
+  }
+
+  // Handle the webhook
+  const eventType: EventType = evt.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    console.log("user created or updated", evt.data)
+    const { id, ...attributes } = evt.data;
+    await prisma.user.upsert({
+        where: { clerkUserId: id as string},
+        create: {
+          clerkUserId: id as string,
+          // Set other fields from evt.data if needed
+        },
+        update: {
+          // Update other fields from evt.data if needed
+        },
+      });
+      
+  }
+
+  res.json({});
+}
+
+type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+};
+
+// Generic (and naive) way for the Clerk event
+// payload type.
+type Event = {
+  data: Record<string, string | number>;
+  object: "event";
+  type: EventType;
+};
+
+type EventType = "user.created" | "user.updated" | "*";
