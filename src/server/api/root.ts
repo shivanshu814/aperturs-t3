421cbe4b1c6cc06d03473285adf95dee4a6b2ec1
swaraj bachu <rajswaraj.r@gmail.com>
2023-06-26 19:59:06 +0530

    created protected procedure, added user to context

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -5,7 +5,9 @@ import { createTRPCRouter } from "~/server/api/trpc";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+        
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
