34a790e5d65f1a2afd12af8763f56ffdec42c294
swaraj bachu <rajswaraj.r@gmail.com>
2023-06-28 15:43:48 +0530

    little schma changes and twitter trpc

---
+++ src/server/api/routers/twitter/twitter-data.tsx
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../../trpc";
+
+export const twitterOrgAuth = createTRPCRouter({
+  createUser: protectedProcedure
+    .input(
+      z.object({
+        clientId: z.string(),
+        clientSecret:z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.twitterToken.create({
+        data: {
+            client_id: input.clientId,
+            client_secret:input.clientSecret,
+            clerkUserId: ctx.currentUser.id
+        },
+      });
+      return user;
+    }),
+});
